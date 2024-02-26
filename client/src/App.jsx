2ee561b9aa31d24cea16b0333cda622be89f2487
edm1001 <edmer_franciz@yahoo.com>
2024-02-26 17:05:59 -0600

    init register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,9 @@
-// import { useState } from 'react'
-
-import './App.css'
+import Register from "./Register"
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
-    <div>
-      test
-    </div>
+    <Register/>
   )
 }
 
