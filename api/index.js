1e71148d7d5df171dcdaf208489291745e3b1d69
edm1001 <edmer_franciz@yahoo.com>
2024-02-27 20:17:26 -0600

    init user model

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
 // send the info here 
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+
 
 app.get('/test', (req, res) => {
-    res.json('test')
+    res.json('test OK')
 });
+
+app.post('/register' , (req,res) => {
+
+})
+
+app.listen(4000);
