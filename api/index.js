36bc0f5b8ba2abc1c623915fc88ddb354227d82a
edm1001 <edmer_franciz@yahoo.com>
2024-03-04 18:12:16 -0600

    add password security for registering

--- api/index.js
+++ api/index.js
@@ -2,18 +2,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User.js');
 
-const app = express();
 dotenv.config();
+mongoose.connect(process.env.MONGO_URL);
+const jwtSecret = process.env.JWT_SECRET;
+const app = express();
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}))
 
-mongoose.connect(process.env.MONGO_URL)
 
 
 app.get('/test', (req, res) => {
     res.json('test OK')
 });
 
-app.post('/register' , (req,res) => {
+app.post('/register' ,async (req,res) => {
+    const {username, password} = req.body;
+    try {
+        const createdUser = await User.create({username, password});
+        jwt.sign({userId:createdUser._id},jwtSecret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).status(201).json({
+                _id: createdUser._id,
+            });
+        })
+        
+    }catch(err) {
+        if (err) throw err;
+        res.status(500).json('error');
+    }
 
 })
 
