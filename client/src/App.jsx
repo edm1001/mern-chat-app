86c262c64d5f26de46f17cf4c733e1ded168d903
edm1001 <edmer_franciz@yahoo.com>
2024-03-05 22:09:36 -0600

    init user context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import Register from "./Register"
 import axios from 'axios';
+import { UserContextProvider, UserContext } from "./UserContext";
+import { useContext } from "react";
 
 function App() {
   axios.defaults.baseURL = 'http://localhost:4000';
   axios.defaults.withCredentials = true;
+  const {username} = useContext{UserContext}
   return (
+    <UserContextProvider>
     <Register/>
+    </UserContextProvider>
   )
 }
 
