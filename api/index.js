12c07a6990c2c23d8e60fe58052048e25e148774
edm1001 <edmer_franciz@yahoo.com>
2024-03-12 20:25:29 -0500

    working login and registration

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("js");
+const bcrypt = require("bcryptjs");
 const User = require("./models/User.js");
 const cookieParser = require("cookie-parser");
 
@@ -39,13 +39,25 @@ app.get("/profile", (req, res) => {
   }
 });
 
-app.post('/login', async (req,res) => {
-  const {username, password} = req.body;
-  const foundUser = await User.find({
-    username, 
-    password: 
-  });
-})
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const foundUser = await User.findOne({ username });
+  if (foundUser) {
+    const passOk = bcrypt.compareSync(password, foundUser.password);
+    if (passOk) {
+      jwt.sign(
+        { userId: foundUser._id, username },
+        jwtSecret,
+        {},
+        (err, token) => {
+          res.cookie("token", token, { sameSite: "none", secure: true }).json({
+            id: foundUser._id,
+          });
+        }
+      );
+    }
+  }
+});
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
