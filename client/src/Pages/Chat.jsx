59e9b46f14d73963c297bdf14dd992e6a8b17402
edm1001 <edmer_franciz@yahoo.com>
2024-03-19 15:56:31 -0500

    init file messages

--- client/src/Pages/Chat.jsx
+++ client/src/Pages/Chat.jsx
@@ -57,13 +57,15 @@ export default function Chat() {
       setMessages((prev) => [...prev, { ...messageData }]);
     }
   }
-  function sendMessage(ev) {
-    ev.preventDefault();
+  function sendMessage(ev, file = null) {
+    if (ev) ev.preventDefault();
     ws.send(
       JSON.stringify({
         recipient: selectedUserId,
         text: newMessage,
+        file
       })
+
     );
     setNewMessage("");
     setMessages((prev) => [
@@ -76,6 +78,17 @@ export default function Chat() {
       },
     ]);
   }
+  function sendFile(ev) {
+    const reader = new FileReader();
+    reader.readAsDataURL(ev.target.files[0]);
+    reader.onload = () => {
+      sendMessage(null, {
+        info: ev.target.files[0],
+        data: reader.result,
+      })
+    }
+  }
+
   useEffect(() => {
     const div = divUnderMessages.current;
     if (div) {
@@ -205,9 +218,23 @@ export default function Chat() {
               placeholder="Type message here"
               className="bg-white flex-grow border rounded-sm p-2"
             />
-            <button type="button">
-              
-            </button>
+
+            <label className="bg-gray-500 p-2 text-white border border-gray-500 rounded-sm cursor-pointer">
+              <input type="file" className="hidden" onChange={sendFile}/>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M18.97 3.659a2.25 2.25 0 0 0-3.182 0l-10.94 10.94a3.75 3.75 0 1 0 5.304 5.303l7.693-7.693a.75.75 0 0 1 1.06 1.06l-7.693 7.693a5.25 5.25 0 1 1-7.424-7.424l10.939-10.94a3.75 3.75 0 1 1 5.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 0 1 5.91 15.66l7.81-7.81a.75.75 0 0 1 1.061 1.06l-7.81 7.81a.75.75 0 0 0 1.054 1.068L18.97 6.84a2.25 2.25 0 0 0 0-3.182Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </label>
+
             <button
               type="submit"
               className="bg-blue-500 p-2 text-white rounded-sm"
